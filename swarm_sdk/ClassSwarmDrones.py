cb167aacf75f1d7908522435ed07c4ed1735c692
OZ77ARIEL <82040014+OZ77ARIEL@users.noreply.github.com>
2023-08-08 16:59:26 +0300

    a

--- swarm_sdk/ClassSwarmDrones.py
+++ swarm_sdk/ClassSwarmDrones.py
@@ -1,9 +1,12 @@
 import numpy as np
 from vpython import *
 from exstract_point import *
-import keyboard
 from swarm import *
+from find_location import *
 import time
+import os
+import shutil
+
 
 swarm_sdk = Swarm()
 
@@ -21,11 +24,15 @@ class Drone:
 
 
     def takeoff(self):
+        #self.streamon()
+    
+        print('checkpoint takeoff of ', self.id-1)
         if not self.sim:
             # takeoff the drone
-            print('checkpoint takeoff')
             swarm_sdk.commands = [str(self.id) + '>takeoff']
             swarm_sdk.start()
+            
+
 
     def move_to(self, next_pos):
     # Move the drone to the next position
@@ -42,6 +49,12 @@ class Drone:
         if self.sim:
             time.sleep(0.5)
     
+    def streamon(self):
+        if not self.sim:
+            swarm_sdk.commands = [str(self.id) + '>streamon']
+            swarm_sdk.start()
+
+
     def land(self):
         if not self.sim:    
             # land the drone
@@ -58,7 +71,10 @@ class SwarmDronesMove:
         self.drones = []
         self.start_mode=True
         self.v = v
+        self.GRREEN_WIDTH=10
+        self.GREEN_HIGH=8
 
+        self.find_loc_of=np.zeros((num_drone),dtype=np.uint8)
         swarm_sdk.commands = ['scan ' + str(num_drone)]
         swarm_sdk.start()
 
@@ -72,17 +88,24 @@ class SwarmDronesMove:
             Vp_point = sphere(pos=vector(point[0],point[1], 0), radius=7, color=color.red)
             self.points.append(Vp_point) 
             
-        for i in range(num_drone):
-            print('first for = ', i)
-            drone = Drone(self.start_pos_list[i], i+1, self.v)
-            drone.next_pos = self.points[0].pos
-            self.drones.append(drone)
+        for i in range(num_drone+num_sim_drone):
+            if i < num_drone:
+                print('drone num = ', i)
+                drone = Drone(self.start_pos_list[i], i+1, self.v)
+                drone.next_pos = self.points[0].pos
+                self.drones.append(drone)
         
-        for j in range(num_drone,num_drone+num_sim_drone):
-            #print(' for = ', j+1)
-            drone = Drone(self.start_pos_list[num_drone+j], -1 ,self.v, sim=True)
-            drone.next_pos = self.points[0].pos
-            self.drones.append(drone)
+            else:
+                print(' sim drone = ', i)
+                drone = Drone(self.start_pos_list[i], -1 ,self.v, sim=True)
+                drone.next_pos = self.points[0].pos
+                self.drones.append(drone)
+
+        # create folder for each drone if already exists delet it and create new one
+        for i in range(num_drone):
+            if os.path.exists(f'./img_drone{i}'):
+                shutil.rmtree(f'./img_drone{i}')
+            os.mkdir(f'./img_drone{i}')
 
     def start_move(self):
     # Move the drones to their respective start points
@@ -90,13 +113,12 @@ class SwarmDronesMove:
         for idx , this_drone in enumerate(self.drones):
             in_the_start_point=False
             while (in_the_start_point==False):
-                if keyboard.is_pressed('q'):
-                    self.stop()
                     
                 print('in start move before takeoff = ', this_drone.start )
                 if (this_drone.start==False):
-                    this_drone.takeoff()
-                
+                    this_drone.takeoff() 
+                    this_drone.start=True
+
                 this_drone.point_pos = (this_drone.point_pos+1) % self.num_point
                 this_drone.move_to(self.points[this_drone.point_pos].pos)
 
@@ -106,19 +128,16 @@ class SwarmDronesMove:
         self.start_mode=False
 
     def move_drones(self):
-        #if push on kay 'q' stop the program
-        if keyboard.is_pressed('q'):
-            self.stop()
 
+        print("start move drones")
         for this_drone in self.drones:  #need to be simultanius
             this_drone.point_pos = (this_drone.point_pos+1) % self.num_point
             this_drone.move_to(self.points[this_drone.point_pos].pos)
 
+
     def move(self):
         while(True):
-            if keyboard.is_pressed('q'):
-                self.stop()
-
+            
             if (self.start_mode):  
                 self.start_move()
             else:
@@ -129,7 +148,6 @@ class SwarmDronesMove:
             if not(this_drone.sim):
                 this_drone.find_location()
             
-
 
     def move_point(self,direction,v):
     # Move all points in the given direction
